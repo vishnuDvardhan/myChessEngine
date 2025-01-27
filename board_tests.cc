7b65ca7449fdaf8871f1456675e8843683faad56
vishnuDvardhan <vishnudvardhan1@gmail.com>
2025-01-27 20:22:42 +0530

    added tests for check case in correct turn generation

--- board_tests.cc
+++ board_tests.cc
@@ -75,6 +75,15 @@ TEST(TurnGenerationsTests, GenerateCorrectTurnTests) {
       {{1, 4}, {2, 4}}, {{1, 4}, {3, 4}}, {{1, 5}, {2, 5}}, {{1, 5}, {3, 5}},
       {{1, 6}, {2, 6}}, {{1, 6}, {3, 6}}, {{1, 7}, {2, 7}}, {{1, 7}, {3, 7}}};
   ASSERT_EQ(allTurnsActual, allTurnsExpected);
+  currentState.board[2][3] = Pieces::bBishop;
+  allTurnsExpected = {
+      {{0, 2}, {2, 3}}, {{0, 2}, {2, 1}}, {{0, 5}, {2, 6}}, {{0, 5}, {2, 4}},
+      {{1, 0}, {2, 0}}, {{1, 0}, {3, 0}}, {{1, 1}, {2, 1}}, {{1, 1}, {3, 1}},
+      {{1, 2}, {2, 2}}, {{1, 2}, {3, 2}}, {{1, 2}, {2, 3}}, {{1, 4}, {2, 4}},
+      {{1, 4}, {3, 4}}, {{1, 4}, {2, 3}}, {{1, 5}, {2, 5}}, {{1, 5}, {3, 5}},
+      {{1, 6}, {2, 6}}, {{1, 6}, {3, 6}}, {{1, 7}, {2, 7}}, {{1, 7}, {3, 7}}};
+  allTurnsActual = generateTurns(Colours::white);
+  ASSERT_EQ(allTurnsActual, allTurnsExpected);
 }
 
 int main(int argc, char **argv) {
